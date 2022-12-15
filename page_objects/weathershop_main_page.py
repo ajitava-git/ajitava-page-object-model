dae3b9998d22251e045eb64e61643aa13c084b58
ajitava <ajitava.deb@qxf2.com>
2022-12-16 00:28:44 +0530

    modified a variable and added comments

--- page_objects/weathershop_main_page.py
+++ page_objects/weathershop_main_page.py
@@ -23,31 +23,34 @@ class WeatherShop_Main_Page(Base_Page):
 
     def check_temperature(self):
 
-        value = 19
-        
+        temperature_value = 19
+
         result_flag = self.check_element_present(self.temperature)
         self.log_result(result_flag,
             positive='Temperature is present',
             negative='Temperature not present',
             level='debug')
 
+        #Finding the element in the DOM
         result_element = self.get_element(self.temperature,verbose_flag=True)
         print(result_element)
 
+        #Extracting the text from DOM element
         result_element_text = self.get_dom_text(result_element)
         print(result_element_text)
 
+        #Decoding the encoded text and removing the °C/℃ to get the current dynamic temperature
         current_temperature = result_element_text.decode('utf-8').replace('°C','').replace('℃','')
         print(current_temperature)
 
-        if int(current_temperature) < value:
+        if int(current_temperature) < temperature_value:
             result_flag = self.check_element_present(self.moisturizer)
             self.log_result(result_flag,
                 positive='Moisturizer is present',
                 negative='Moisturizer is not present',
                 level='debug')
             self.click_element(self.moisturizer)
-        elif int(current_temperature) > value:
+        elif int(current_temperature) > temperature_value:
             result_flag = self.check_element_present(self.sunscreen)
             self.log_result(result_flag,
                 positive='sunscreen is present',
